457b200c7e92259c13f172798a27e3404e642414
Brayan <brastevendelgo@gmail.com>
2024-10-23 20:17:42 -0500

    Graficas de datos titanic

--- AD.py
+++ AD.py
@@ -13,26 +13,97 @@ datos[3]=6
 print (datos)
 sumary_line
 
-"""
+
 
 #----------------------------------------------------------------
 import pandas as pd
 #Crear un data frame(arreglo de datos)
 datos = {
-    'Nombre':['Andres', 'Carlos', 'Maria', 'Juan'],
-    'Edad':[20,21,25,12],
-    'Nota':[3.5,3.0,4.5,3.9]
+    'Nombre':['Andres', 'Carlos', 'Maria', 'Juan', 'Jaime'],
+    'Edad':[20,21,25,12,13],
+    'Carrera':['Sitemas','Electronica', 'Electronica','Sistemas', 'Sistemas']    
 }
-df=pd.DataFrame(datos)
-#print(df)
+"""
+"""
+#Imputacioón de datos
+import pandas as pd
+datos = {
+    'Nombre':['Andres', 'Carlos', 'Maria', 'Juan', 'Jaime'],
+    'Edad':[20,None,25,None,13],
+    'Carrera':['Sitemas','Electronica', 'Electronica','Sistemas', 'Sistemas']    
+}
 
+#rellenar datos faltantes .fillna con la media de datos
+dfnull=pd.DataFrame(datos)
+dfnull['Edad'] = dfnull ['Edad'].fillna (dfnull['Edad'].mean())
+print(dfnull)
+"""
+import pandas as pd
+df=pd.read_csv('http://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+print(df.head())
+
+#pERSONAS QUE SOBREVIVIERON
+survivor=df[df['Survived']== 1]
+print(survivor)
+#Personas que sobrevivieron mayor de 60años
+survivor_60=df[(df['Survived']== 1)&(df['Age']>60)]
+print(survivor_60)
+
+#Promedio de muertos
+muertos=df[df['Survived']== 0]
+print(muertos)
+
+#Grafica de sobrevivientes por genero
+import matplotlib.pyplot as plt
+import seaborn as sbn 
+
+sbn.countplot(x='Sex', hue='Survived', data=df)
+plt.title('Grafico de sobrevivencia vs edad')
+plt.show()
+"""
+#Base de datos sin valores nulos (dropna)
+dfnull=pd.DataFrame(datos)
+df_sin_nulos=dfnull.dropna(subset=['Edad'])
+print(df_sin_nulos)
+
+"""
+
+"""
+
+dfnull=pd.DataFrame(datos)
+#bASE DE DATOS SIN VALORES NULOS
+df_sin_nulos=dfnull.dropna(subset=['Edad'])
+print(df_sin_nulos)
+
+
+"""
+
+#----------------------------------------------------------------
+#Estudiantes de sistemas de mas de 21 años de edad
+#sis_edad=df(df['Carrera']=='Sistemas'])&(df['edad']>21)]
+
+#Imprimir los estudiantes de sistemas
+#df=pd.DataFrame(datos)
+#Sistemas = df[df['Carrera']=='Sistemas']
+#print (Sistemas)
+              
+#----------------------------------------------------------------
+#Promedio de la edad
+#df=pd.DataFrame(datos)
+#promedio=df['Edad'].mean()
+#print(promedio)
+#----------------------------------------------------------------
+#print(df['Nombre'])
+#print(df)
+#----------------------------------------------------------------
 #cual es el promedio de la edad
-media=df['Edad'].mean()
+#media=df['Edad'].mean()
 #print(media)
 
+#----------------------------------------------------------------
 #Cual es la nota promedio
-media_cal=df['Nota'].mean()
-print(media_cal)
+#media_cal=df['Nota'].mean()
+#print(media_cal)
 #calificación >4
-cal_4 = df[df['Nota']>4]
-print(cal_4)
+#cal_4 = df[df['Nota']>4]
+#print(cal_4)
